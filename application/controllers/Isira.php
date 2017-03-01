8ce84b50e2dcb6ca1ef4f20f64bbc5c38fe61f72
Isira Samaraweera <isira@vesess.com>
2017-03-01 14:31:21 +0530

    First commit

---
+++ application/controllers/Isira.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Isira extends CI_Controller {
+	
+    public function index()
+    {
+      $this->load->view('home_view');          
+    }
+
+     public function kasun()
+    {
+      $this->load->view('home_view');          
+    }
+}
+
---
+++ application/views/home_view.php
@@ -0,0 +1,10 @@
+<html>
+	<head> 
+		<title> code igniter </title>	
+	</head>
+<body>
+<center>
+	<h1> First Code Igniter </h1>
+</center> 
+</body>
+</html>
