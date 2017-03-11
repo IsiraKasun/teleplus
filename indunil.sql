cea0363610f31ce30bcbbf8c9642eafd26b9372a
Isira Samaraweera <isira@vesess.com>
2017-03-11 19:46:48 +0530

    Modified SQL queries by adding feedback table

--- indunil.sql
+++ indunil.sql
@@ -37,11 +37,21 @@ FOREIGN KEY (csf_id) REFERENCES customer_service_staff(csf_id)
 
 CREATE TABLE customers (
 cus_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+cus_name VARCHAR(100),
 cus_detail_URL VARCHAR(255),
-con_num VARCHAR(10),
-csf_id INT,
-FOREIGN KEY (csf_id) REFERENCES customer_service_staff(csf_id)
+con_num VARCHAR(10)
 );
+
+CREATE TABLE feedback (
+
+f_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+status VARCHAR(10),
+pro_id INT,
+cus_id INT,
+FOREIGN KEY (pro_id) REFERENCES promotion(pro_id),
+FOREIGN KEY (cus_id) REFERENCES customers(cus_id)
+);
+
 
 CREATE TABLE agreement (
 agg_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -69,4 +79,10 @@ FOREIGN KEY (dep_id) REFERENCES department(dep_id)
 );
 
 INSERT INTO system_user (name, password) VALUES ('isira', sha1('abc123'));
-INSERT INTO administrator VALUES ('1');
+INSERT INTO administrator VALUES ('1'); 
+
+INSERT INTO system_user (name, password) VALUES ('indunil', sha1('abc123'));
+INSERT INTO customer_service_staff VALUES('2');
+
+INSERT INTO promotion (type,Description,Starting_date,Ending_date,csf_id) VALUES ('campaign','4G Connection','2017-03-11','2017-04-11','2');
+INSERT INTO customers (cus_name,cus_detail_URL,con_num) VALUES ('Ashali','www.ashali.com','0719828198');
