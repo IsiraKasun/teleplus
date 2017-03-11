3bb1e7c4cf52b8125dc5b84819a9887bca9b10ef
Isira Samaraweera <isira@vesess.com>
2017-03-11 22:41:38 +0530

    completed promotion adding with validation

--- application/controllers/Promotion_controller.php
+++ application/controllers/Promotion_controller.php
@@ -26,7 +26,12 @@ class Promotion_controller extends CI_Controller {
     public function add_new_promotion()
     {		
 
-    		 if (isset($_SESSION['current_user'])){
+    	$this->form_validation->set_rules('type', 'Type', 'trim|required|min_length[2]|max_length[10]');
+    	$this->form_validation->set_rules('description', 'Description', 'trim|required|min_length[2]|max_length[255]');
+    	$this->form_validation->set_rules('starting_date', 'Starting Date', 'trim|required|min_length[2]|max_length[10]');
+    	$this->form_validation->set_rules('ending_date', 'Ending Date', 'trim|required|min_length[2]|max_length[10]');
+    	if($this->form_validation->run())  {
+    			if (isset($_SESSION['current_user'])){
     				$data['current_user'] = $_SESSION['current_user'];
     				$sdate = $this->input->post('starting_date');
     				$edate = $this->input->post('ending_date');
@@ -48,6 +53,18 @@ class Promotion_controller extends CI_Controller {
 					$data['loginerror'] = "Username or password incorrect";
 	      	 $this->load->view('login_view',$data); 
     			}
+    	}
+
+    	else{
+    			$data['current_user'] = $_SESSION['current_user'];
+    			$this->load->model('promotion_model');  
+			    $data['values']=$this->promotion_model->get_promotion_data();  
+			    $this->load->view('promotion_view',$data); 
+    	}
+
+    		 
+
+    			
 
     	 
     }
